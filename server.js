1e15730a7a6c54363051d9e484272bd6f895f9cf
AnthonySzymczak <doyalajs1@live.com>
2021-08-02 13:44:42 -0400

    adding pathing

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
+const path = require ('path');
 
 const app = express();
 const PORT = process.env.PORT || 3021;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 
 app.use(routes);
 
