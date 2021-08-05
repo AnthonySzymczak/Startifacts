3da056fd25a3ce39cae0c7ed2798f9541de861dc
AnthonySzymczak <doyalajs1@live.com>
2021-08-05 16:47:16 -0400

    class time

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -12,15 +12,7 @@ router.get("/:id", (req, res) => {
   User.findOne({
     attributes: { exclude: ["password"] },
     where: { id: req.params.id },
-    include: [
-      { model: Post, attributes: ["id", "title", "content", "created_at"] },
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "created_at"],
-        include: { model: Post, attributes: ["title"] },
-      },
-      { model: Post, attributes: ["title"] },
-    ],
+    
   })
     .then((dbUserData) => {
       if (!dbUserData) {
