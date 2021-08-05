fbe1bfbb1211e24f6ae50bacb703d8628f2b7a2f
AnthonySzymczak <doyalajs1@live.com>
2021-08-04 21:37:27 -0400

    heroku

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const hbs = exphbs.create();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+  const SequelizeStore = require('connect-session-sequelize')(session.Store);
+  const sess = {
+  secret: process.env.SERVER_SECRET,
+  cookie: { scpNum: 4400 }, 
+  resave: false, 
+  saveUninitialized: true, 
+  store: new SequelizeStore({ 
+  db: sequelize  
+  })
+  };
+     app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
