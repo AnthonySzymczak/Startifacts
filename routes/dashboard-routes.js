8119d6487a5bee04a3ab52981b6f5ce8543b9120
Manny Alfaro <mannyalfa@gmail.com>
2021-08-08 11:07:45 -0400

    created dashboard saved item routes

--- routes/dashboard-routes.js
+++ routes/dashboard-routes.js
@@ -1,76 +1,36 @@
-const sequelize = require("../config/connection");
-const { Post, User, Comment } = require("../models");
-const withAuth = require("../utils/auth");
-const router = require("express").Router();
-/*router.get("/", withAuth, (req, res) => {
-  Post.findAll({
-    where: {
-      user_id: req.session.user_id,
-    },
-    attributes: ["id", "title", "content", "created_at"],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag} = require('../models');
+
+//get shop cards
+router.get('/', (req, res) => {
+  console.log(req.session)
+  // if (req.session) 
+  console.log(req.session.loggedIn)
+
+    Product.findAll({
+        attributes: ['id','product_image', 'product_name', 'price', 'stock', 'category_id'],
+        include: [{
+          model: Category,
+          attributes: ['id', 'category_name']
         },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("dashboard", { posts, loggedIn: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-router.get("/edit/:id", withAuth, (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id,
-    },
-    attributes: ["id", "title", "content", "created_at"],
-    include: [
-      {
-        model: User,
-        attributes: ["username"],
-      },
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
+        {
+          model: Tag,
+          through: ProductTag,
+          as: 'product_tag'
         },
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (!dbPostData) {
-        res.status(404).json({ message: "No post found with this id" });
-        return;
-      }
-
-      const post = dbPostData.get({ plain: true });
-      res.render("edit-post", { post, loggedIn: true });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json(err);
+        ],
+      })
+    .then(dbProducts => {
+      const products = dbProducts.map(product => product.get({ plain: true }));
+    
+      if(req.session.loggedIn)
+      res.render('shop', { products, loggedIn: true });
+      else
+      res.render('shop', {products})
     });
-});*/
-router.get("/", withAuth, (req, res) => {
-  res.render("dashboard", {loggedIn: true});
-});
+  });
 
 
 
-module.exports = router;
+  module.exports = router;
+  
